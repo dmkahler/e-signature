93af677c4020fc90da5d01197a04be3aecc1e14d
David Kahler <david.m.kahler@gmail.com>
2022-01-07 14:44:56 -0500

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "another test, with batch command"
+Rscript esign.R "USAID ADS303 certification of no narcotics or drug offenses"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
