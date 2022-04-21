4f72b4a9d4a44496d82669f381fdd4629bf742ff
David Kahler <david.m.kahler@gmail.com>
2022-04-21 10:44:07 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Request for data from CENCARTA"
+Rscript esign.R "Request for data from ARA-SOL"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
