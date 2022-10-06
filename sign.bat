c72693747a071e081d86a46487a60ac879fde319
David Kahler <david.m.kahler@gmail.com>
2022-10-06 15:10:34 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Kahler - 2022 effort certification"
+Rscript esign.R "Bakar - 2022 effort certification as PI"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
