02598f31a8a9ed89c12789f273849b5f91cd9133
David Kahler <david.m.kahler@gmail.com>
2022-10-06 16:00:19 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Bakar - 2022 effort certification as PI"
+Rscript esign.R "2022 conflict of interest disclosure"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
