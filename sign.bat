f7e57ed158b915631c6a30ac8c851a46d5bb8b20
David Kahler <david.m.kahler@gmail.com>
2025-04-17 07:06:19 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "SAN Parks data agreement for crayfish, macroinvert, water physicoshemical"
+Rscript esign.R "Fortsch URP agreement"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
