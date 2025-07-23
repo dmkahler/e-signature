b28c0c3e9c16d0faf0f0e6dd7e14f17814c863a0
David Kahler <david.m.kahler@gmail.com>
2025-07-23 09:56:24 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Ranii, English - Certified"
+Rscript esign.R "Rubright, English - Certified"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
