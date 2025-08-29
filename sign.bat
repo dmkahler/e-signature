aaf98914bbfc0db9eb7cd50952b330ba5cc0bdb7
David Kahler <david.m.kahler@gmail.com>
2025-08-29 14:24:00 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Reeder ClaimsConf - D. Johnson"
+Rscript esign.R "Freer 3 credits of research"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
