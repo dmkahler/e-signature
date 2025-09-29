b107174bf46c7b5f2fa9b4ef61a7081d031d95b1
David Kahler <david.m.kahler@gmail.com>
2025-09-29 15:14:04 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Meding 1 credit of research"
+Rscript esign.R "AY2024-25 FAE Dean's response - notes made"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
