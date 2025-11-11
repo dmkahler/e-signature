555c42f699591638d4e797a6801a925b42187775
David Kahler <david.m.kahler@gmail.com>
2025-11-11 13:48:59 -0500

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Ferrick English - certified"
+Rscript esign.R "Robinson, GIS Spring 2026"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
