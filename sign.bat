8a869c584aeb7ed1196049d9d243c8b12221eea3
David Kahler <david.m.kahler@gmail.com>
2026-03-30 12:01:35 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Wheeler chair 2026 lab volunteer"
+Rscript esign.R "Bathgate contract request"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
