40c6d5687fd86afa72eb649b5d9ba90660f19cd9
David Kahler <david.m.kahler@gmail.com>
2026-04-26 22:43:50 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Sweeney 2026 URP faculty approval"
+Rscript esign.R "Janecka summer 2026 sampling contract request"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
