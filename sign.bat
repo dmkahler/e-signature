fc4e44874c22f6594654f453aa908a7a29932e86
David Kahler <david.m.kahler@gmail.com>
2026-04-29 13:59:16 -0400

    updated log

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Rscript esign.R "Dakin summer 2026 sampling contract request"
+Rscript esign.R "Kahler summer 2026 computer course contract request"
 git add verificationlog.csv
 git add sign.bat
 git commit -m "updated log"
